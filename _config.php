1301d2bd6d21fa062a452578234ec48a9e12b6e2
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-06-18 14:59:16 +0530

    config fixed

--- _config.php
+++ _config.php
@@ -5,9 +5,9 @@ use ImportExport\Extensions\ImportAdminExtension;
 use SilverStripe\Core\Config\Config;
 
 ModelAdmin::add_extension(ImportAdminExtension::class);
-$remove = Config::inst()->get('ModelAdmin','removelegacyimporters');
+$remove = Config::inst()->get(ModelAdmin::class,'removelegacyimporters');
 if($remove === "scaffolded"){
-    Config::inst()->update("ModelAdmin", 'model_importers', array());
+    Config::modify()->set(ModelAdmin::class, 'model_importers', array());
 }
 //cache mappings forever
 //Cache::set_cache_lifetime('gridfieldimporter', null); //this want to improve
