3bae28dab71d7af7df1cc1b8a24f7f70e6689f43
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-11-25 16:39:41 +0530

    accedental error fix

--- code/Gridfield/GridFieldImporter_Request.php
+++ code/Gridfield/GridFieldImporter_Request.php
@@ -1,190 +1,359 @@
 <?php
 
 /**
- * Adds a way to import data to the GridField's DataList
+ * Request handler that provides a seperate interface
+ * for users to map columns and trigger import.
  */
 namespace ImportExport\Gridfield;
 
-use ImportExport\Bulkloader\BetterBulkLoader;
-use ImportExport\Bulkloader\Sources\CsvBulkLoaderSource;
+use ImportExport\CSVFieldMapper;
+use Psr\SimpleCache\CacheInterface;
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\File;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Control\RequestHandler;
+use SilverStripe\Core\Convert;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Dev\BulkLoader_Result;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\GridField\GridField_FormAction;
-use SilverStripe\Forms\GridField\GridField_HTMLProvider;
 use SilverStripe\Forms\GridField\GridField_URLHandler;
-use SilverStripe\ORM\HasManyList;
+use SilverStripe\Forms\GridField\GridFieldDetailForm_ItemRequest;
+use SilverStripe\Forms\HiddenField;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\View\ArrayData;
-use SilverStripe\View\Requirements;
 
-class GridFieldImporter implements GridField_HTMLProvider, GridField_URLHandler
+class GridFieldImporter_Request extends RequestHandler
 {
 
     /**
-     * Fragment to write the button to
-     * @var string
+     * Gridfield instance
+     * @var GridField
      */
-    protected $targetFragment;
+    protected $gridField;
 
     /**
-     * The BulkLoader to load with
-     * @var string
+     * The parent GridFieldImporter
+     * @var GridFieldImporter
      */
-    protected $loader = null;
+    protected $component;
 
     /**
-     * Can the user clear records
-     * @var boolean
+     * URLSegment for this request handler
+     * @var string
      */
-    protected $canClearData = true;
+    protected $urlSegment = 'importer';
 
-    public function __construct($targetFragment = "after")
+    /**
+     * Parent handler to link up to
+     * @var RequestHandler
+     */
+    protected $requestHandler;
+
+    /**
+     * RequestHandler allowed actions
+     * @var array
+     */
+    private static $allowed_actions = array(
+        'preview', 'upload', 'import'
+    );
+
+    /**
+     * RequestHandler url => action map
+     * @var array
+     */
+    private static $url_handlers = array(
+        'upload!' => 'upload',
+        '$Action/$FileID' => '$Action'
+    );
+
+    /**
+     * Handler's constructor
+     *
+     * @param GridField $gridField
+     * @param GridField_URLHandler $component
+     * @param RequestHandler $handler
+     */
+    public function __construct($gridField, $component, $handler)
     {
-        $this->targetFragment = $targetFragment;
+        $this->gridField = $gridField;
+        $this->component = $component;
+        $this->requestHandler = $handler;
+        parent::__construct();
     }
 
     /**
-     * Set the bulk loader for this importer
-     * @param BetterBulkLoader $loader
-     * @return GridFieldImporter
+     * Return the original component's UploadField
+     *
+     * @return UploadField UploadField instance as defined in the component
      */
-    public function setLoader(BetterBulkLoader $loader)
+    public function getUploadField()
     {
-        $this->loader = $loader;
-
-        return $this;
+        return $this->component->getUploadField($this->gridField);
     }
 
     /**
-     * Get the BulkLoader
-     * @return BetterBulkLoader
+     * Upload the given file, and import or start preview.
+     * @param  HTTPRequest $request
+     * @return string
      */
-    public function getLoader(GridField $gridField)
+    public function upload(HTTPRequest $request)
     {
-        if (!$this->loader) {
-            $this->loader = $this->scaffoldLoader($gridField);
+        $field = $this->getUploadField();
+        $uploadResponse = $field->upload($request);
+        //decode response body. ugly hack ;o
+        $body = json_decode($uploadResponse->getBody());
+        $body = array_shift($body);
+        //add extra data
+        $body['import_url'] = Controller::join_links(
+            $this->Link('preview'), $body->id,
+            // Also pull the back URL from the current request so we can persist this particular URL through the following pages.
+            "?BackURL=" . $this->getBackURL($request)
+        );
+        //don't return buttons at all
+        unset($body['buttons']);
+        //re-encode
+        $response = new HTTPResponse(json_encode(array($body)));
+
+        return $response;
+    }
+
+    /**
+     * Action for getting preview interface.
+     * @param  HTTPRequest $request
+     * @return string
+     */
+    public function preview(HTTPRequest $request)
+    {
+        $file = File::get()
+            ->byID($request->param('FileID'));
+        if (!$file) {
+            return "file not found";
+        }
+        //TODO: validate file?
+        $mapper = new CSVFieldMapper($file->getFullPath());
+        $mapper->setMappableCols($this->getMappableColumns());
+        //load previously stored values
+        if ($cachedmapping = $this->getCachedMapping()) {
+            $mapper->loadDataFrom($cachedmapping);
         }
 
-        return $this->loader;
-    }
-
-    /**
-     * Scaffold a bulk loader, if none is provided
-     */
-    public function scaffoldLoader(GridField $gridField)
-    {
-        $gridlist = $gridField->getList();
-        $class = ($gridlist instanceof HasManyList) ?
-            "ListBulkLoader" : "BetterBulkLoader";
-        //set the correct constructor argument
-        $arg = ($class === "ListBulkLoader" ||
-            is_subclass_of($class, "ListBulkLoader")) ?
-            $gridlist : $gridField->getModelClass();
-        $loader = new $class($arg);
-        $loader->setSource(new CsvBulkLoaderSource());
-
-        return $loader;
-    }
-
-    /**
-     * @param boolean $canClearData
-     */
-    public function setCanClearData($canClearData = true)
-    {
-        $this->canClearData = $canClearData;
-    }
-
-    /**
-     * Get can clear data flag
-     */
-    public function getCanClearData()
-    {
-        return $this->canClearData;
-    }
-
-    /**
-     * Get the html/css button and upload field to perform import.
-     */
-    public function getHTMLFragments($gridField)
-    {
-        $button = new GridField_FormAction(
-            $gridField,
-            'importcsv',
-            _t('TableListField.CSVIMPORT', 'Import from CSV'),
-            'importcsv',
-            null
+        $form = $this->MapperForm();
+        $form->Fields()->unshift(
+            new LiteralField('mapperfield', $mapper->forTemplate())
         );
-        $button->setAttribute('data-icon', 'drive-upload');
-        $button->addExtraClass('no-ajax');
-        $uploadfield = $this->getUploadField($gridField);
-        $data = array(
-            'Button' => $button,
-            'UploadField' => $uploadfield
-        );
-        $importerHTML = ArrayData::create($data)
-            ->renderWith("GridFieldImporter");
-        Requirements::javascript('burnbright/silverstripe-importexport: javascript/GridFieldImporter.js');
+        $form->Fields()->push(new HiddenField("BackURL", "BackURL", $this->getBackURL($request)));
+        $form->setFormAction($this->Link('import').'/'.$file->ID);
+        $content = ArrayData::create(array(
+            'File' => $file,
+            'MapperForm'=> $form
+        ))->renderWith('GridFieldImporter_preview');
+        $controller = $this->getToplevelController();
 
-        return array(
-            $this->targetFragment => $importerHTML
-        );
+        return $controller->customise(array(
+            'Content' => $content
+        ));
     }
 
     /**
-     * Return a configured UploadField instance
-     *
-     * @param  GridField $gridField Current GridField
-     * @return UploadField          Configured UploadField instance
+     * The import form for creating mapping,
+     * and choosing options.
+     * @return Form
      */
-    public function getUploadField(GridField $gridField)
+    public function MapperForm()
     {
-        // check here later this want to update ***
-        $uploadField = UploadField::create(
-            $gridField->Name."_ImportUploadField", 'Upload CSV'
-        )
-            ->setForm($gridField->getForm())
-            ->setSchemaData(
-                ['data' =>
-                    ['createFileEndpoint' =>
-                        [
-                            'url' => $gridField->Link('importer/upload'),
-                            'edit_url' => $gridField->Link('importer/import'),
-                        ],
-                        'maxFilesize' => 1
-                    ]
-                ]
+        $fields = new FieldList(
+            CheckboxField::create("HasHeader",
+                "This data includes a header row.",
+                true
             )
-//            ->setConfig('allowedMaxFileNumber', 1)
-//            ->setConfig('changeDetection', false)
-//            ->setConfig('canPreviewFolder', false)
-//            ->setConfig('canAttachExisting', false)
-//            ->setConfig('overwriteWarning', false)
-            ->setAllowedExtensions(array('csv'))
-            ->setFolderName('csvImports') //TODO: don't store temp CSV in assets
-            ->addExtraClass("import-upload-csv-field");
+        );
+        if ($this->component->getCanClearData()) {
+            $fields->push(
+                CheckboxField::create("ClearData",
+                    "Remove all existing records before import."
+                )
+            );
+        }
+        $actions = new FieldList(
+            new FormAction("import", "Import CSV"),
+            new FormAction("cancel", "Cancel")
+        );
+        $form = new Form($this, __FUNCTION__, $fields, $actions);
 
-        return $uploadField;
+        return $form;
     }
 
-    public function getActions($gridField)
+    /**
+     * Get all columns that can be mapped to in BulkLoader
+     * @return array
+     */
+    protected function getMappableColumns()
     {
-        return array('importer');
+        return $this->component->getLoader($this->gridField)
+                    ->getMappableColumns();
     }
 
-    public function getURLHandlers($gridField)
+    /**
+     * Import the current file
+     * @param  HTTPRequest $request
+     */
+    public function import(HTTPRequest $request)
     {
-        return array(
-            'importer' => 'handleImporter'
+        $hasheader = (bool)$request->postVar('HasHeader');
+        $cleardata = $this->component->getCanClearData() ?
+                         (bool)$request->postVar('ClearData') :
+                         false;
+        if ($request->postVar('action_import')) {
+            $file = File::get()
+                ->byID($request->param('FileID'));
+            if (!$file) {
+                return "file not found";
+            }
+            $colmap = Convert::raw2sql($request->postVar('mappings'));
+            if ($colmap) {
+                //save mapping to cache
+                $this->cacheMapping($colmap);
+                //do import
+                $results = $this->importFile(
+                    $file->getFullPath(),
+                    $colmap,
+                    $hasheader,
+                    $cleardata
+                );
+                $this->gridField->getForm()
+                    ->sessionMessage($results->getMessage(), 'good');
+            }
+        }
+        $controller = $this->getToplevelController();
+        $controller->redirectBack();
+    }
+
+    /**
+     * Do the import using the configured importer.
+     * @param  string $filepath
+     * @param  array|null $colmap
+     * @return BulkLoader_Result
+     */
+    public function importFile($filepath, $colmap = null, $hasheader = true, $cleardata = false)
+    {
+        $loader = $this->component->getLoader($this->gridField);
+        $loader->deleteExistingRecords = $cleardata;
+
+        //set or merge in given col map
+        if (is_array($colmap)) {
+            $loader->columnMap = $loader->columnMap ?
+                array_merge($loader->columnMap, $colmap) : $colmap;
+        }
+        $loader->getSource()
+            ->setFilePath($filepath)
+            ->setHasHeader($hasheader);
+
+        return $loader->load();
+    }
+
+    /**
+     * Pass fileexists request to UploadField
+     *
+     * @link UploadField->fileexists()
+     */
+    public function fileexists(HTTPRequest $request)
+    {
+        $uploadField = $this->getUploadField();
+        return $uploadField->fileexists($request);
+    }
+
+    /**
+     * @param string $action
+     * @return string
+     */
+    public function Link($action = null)
+    {
+        return Controller::join_links(
+            $this->gridField->Link(), $this->urlSegment, $action
         );
     }
 
     /**
-     * Pass importer requests to a new GridFieldImporter_Request
+     * @see GridFieldDetailForm_ItemRequest::getTopLevelController
+     * @return Controller
      */
-    public function handleImporter($gridField, $request = null)
+    protected function getToplevelController()
     {
-        $controller = $gridField->getForm()->getController();
-        $handler    = new GridFieldImporter_Request($gridField, $this, $controller);
+        $c = $this->requestHandler;
+        while ($c && $c instanceof GridFieldDetailForm_ItemRequest) {
+            $c = $c->getController();
+        }
+        if (!$c) {
+            $c = Controller::curr();
+        }
 
-        return $handler->handleRequest($request);
+        return $c;
     }
+
+    /**
+     * Store the user defined mapping for future use.
+     */
+    protected function cacheMapping($mapping)
+    {
+        $mapping = array_filter($mapping);
+        if ($mapping && !empty($mapping)) {
+            $cache = Injector::inst()->get(CacheInterface::class . '.gridfieldimporter');
+            $cache->save(serialize($mapping), $this->cacheKey());
+        }
+    }
+
+    /**
+     * Look for a previously stored user defined mapping.
+     */
+    protected function getCachedMapping()
+    {
+        $cache = Injector::inst()->get(CacheInterface::class . '.gridfieldimporter');
+        if ($result = $cache->load($this->cacheKey())) {
+            return unserialize($result);
+        }
+    }
+
+    /**
+     * Generate a cache key unique to this gridfield
+     */
+    protected function cacheKey()
+    {
+        return md5($this->gridField->Link());
+    }
+
+   /**
+    * Get's the previous URL that lead up to the current request.
+    *
+    * NOTE: Honestly, this should be built into SS_HTTPRequest, but we can't depend on that right now... so instead,
+    * this is being copied verbatim from Controller (in the framework).
+    *
+    * @param HTTPRequest $request
+    * @return string
+    */
+   public function getBackURL() {
+       $request = $this->getRequest();
+      // Initialize a sane default (basically redirects to root admin URL).
+      $controller = $this->getToplevelController();
+      $url = method_exists($this->requestHandler, "Link") ?
+            $this->requestHandler->Link() :
+            $controller->Link();
+
+      // Try to parse out a back URL using standard framework technique.
+      if($request->requestVar('BackURL')) {
+         $url = $request->requestVar('BackURL');
+      } else if($request->isAjax() && $request->getHeader('X-Backurl')) {
+         $url = $request->getHeader('X-Backurl');
+      } else if($request->getHeader('Referer')) {
+         $url = $request->getHeader('Referer');
+      }
+
+      return $url;
+   }
+
 }
